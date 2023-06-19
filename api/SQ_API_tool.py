0c31e6ef1be0e2288d3c1409fc116adb20c78193
Zach Wadhams <zwadhams@gmail.com>
2023-06-19 12:17:24 -0600

    adds incomplete code snippet functionality

--- api/SQ_API_tool.py
+++ api/SQ_API_tool.py
@@ -57,6 +57,26 @@ gitlabHeaders = {
 
 for i in range(len(issueData)): #will create an individual issue post in GitLab for each SQ issue found
 
+    #TODO- look at getting code snippets in GET api/sources/issue_snippets to better fill out description
+    #PENDING SECURITY CONCERNS 
+
+    snippetPayload = {
+        'issueKey': issueData[i].get('key')
+    }
+
+    snippetResponse = requests.get("http://localhost:9000/api/sources/issue_snippets", auth=basicAuth, params=snippetPayload)
+    print("snippetResponse status code:", snippetResponse.status_code)
+
+    snippetData = snippetResponse.json()
+
+    keyList = list(snippetData.keys())
+
+    #the first key needed to get the code is a long string, this gets it easier and stores it for later use as issueFile
+    issueFile = keyList[0]
+    print(issueFile)
+    jprint(snippetData[issueFile])
+
+
     #will get detailed info about the rule that has been violated
     rulesPayload = {
         'key': issueData[i].get('rule')
@@ -67,21 +87,22 @@ for i in range(len(issueData)): #will create an individual issue post in GitLab 
 
     ruleData = ruleInfoResponse.json() if ruleInfoResponse and ruleInfoResponse.status_code == 200 else print("There was an problem with the response. Status code", ruleInfoResponse.status_code)
 
+    #assembles the payload to be sent in the API POST
     gitlabPayload = {
         'id': 46477662, #This is the id of the gitlab repo
         'title': 'SonarQube - {} : {} '.format(issueData[i].get('type').lower(), issueData[i].get('message').lower()),
-        'description': "SonarQube has detected an issue and generated an automatic bug or vulnerability report  \n  \n {} text: '{}' at line {} in file {}  \n Rule Description: {}".format(issueData[i].get('type').lower(), issueData[i].get('message').lower()[:-1], issueData[i].get('line'), re.sub("zwadhams_Embedded-Systems-Robotics_AYibu6FRayQ69Q6kvVmx:", "", issueData[i].get('component')), ruleData['rule']['mdDesc'])
+        'description': "SonarQube has detected an issue and generated an automatic bug or vulnerability report  \n  \n {} text: '{}' at line {} in file {}  \n  \n <h3>Code Snippet</h3>  \n  {}  \n  \n <h2>Rule Description:</h2> {}".format(issueData[i].get('type').lower(), issueData[i].get('message').lower()[:-1], issueData[i].get('line'), re.sub("zwadhams_Embedded-Systems-Robotics_AYibu6FRayQ69Q6kvVmx:", "", issueData[i].get('component')), snippetData['sources'], ruleData['rule']['mdDesc'])
     }
 
     #Compresses the payload into json to avoid a 414 post error 
     jsonPayload = json.dumps(gitlabPayload, indent=1)
 
-    #TODO- look at getting internal api stuff in api/sources/lines to better fill out description
-
     print("-----Posting created issue to GitLab-----")
 #comment the below lines to stop from posting issues, useful to debug
-    issuePost = requests.post("https://gitlab.com/api/v4/projects/46477662/issues",
-                            headers=gitlabHeaders, data=jsonPayload)
-    print("issuePost status code:", issuePost.status_code)
-    if issuePost.status_code == 201:
-        print("-----SUCCESS, GitLab issue created successfully-----")
+    #issuePost = requests.post("https://gitlab.com/api/v4/projects/46477662/issues",
+    #                        headers=gitlabHeaders, data=jsonPayload)
+    #print("issuePost status code:", issuePost.status_code)
+    #if issuePost.status_code == 201:
+    #    print("-----SUCCESS, GitLab issue created successfully-----")
+
+print("SQ/GitLab API Tool Complete")
