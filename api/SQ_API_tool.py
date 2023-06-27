082203e80118bec337c59cb70bcf04fbed98653d
Zach Wadhams <zwadhams@gmail.com>
2023-06-27 11:45:43 -0600

    refactoring how issues are marked in SQ

--- api/SQ_API_tool.py
+++ api/SQ_API_tool.py
@@ -9,7 +9,7 @@ def jprint(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
 
-#function to get issues from json response
+#function to get issues from json response in SQ
 def getIssues(jsonData):
     allIssues = []
     if jsonData and 'issues' in jsonData:
@@ -26,6 +26,7 @@ USER_PASS_SQ = 'apiTesting'
 #simple authentication for sonarqube
 basicAuth = HTTPBasicAuth(USER_AGENT_SQ, USER_PASS_SQ)
 
+"""
 #this block will close out all re-opened issues to clean up the SQ dashboard 
 print("-----Closing Re-Opened SQ Issues")
 
@@ -36,10 +37,8 @@ reOpenedPayload = {
     'severities': 'BLOCKER,CRITICAL',
     'statuses': 'REOPENED'
 }
-
 reOpenedResponse = requests.get("http://localhost:9000/api/issues/search", auth=basicAuth, params=reOpenedPayload)
 print("Re-OpenedResponse status code:", reOpenedResponse.status_code)
-
 reOpenedIssues = getIssues(reOpenedResponse.json())
 
 reOpenedKeys = []
@@ -55,16 +54,18 @@ closePayload = {
 reOpenedClose = requests.post("http://localhost:9000/api/issues/bulk_change", auth=basicAuth, params=closePayload)
 print("reopenedClose status code:", reOpenedClose.status_code)
 
+"""
 #this block will get the issues we want to create issues in GitLab for 
 print("-----Getting data from SonarQube-----")
 print("-----Getting bugs and vulnerabilities with severities of blocker or critical -----")
 
 issuesPayload = { #contains all of the parameters for the get request from sonarqube.
     'componentKeys': 'zwadhams_Embedded-Systems-Robotics_AYibu6FRayQ69Q6kvVmx',
-    'ps': 1, #the number of issue that will be created
+    'ps': 1, #the number of issues that will be created
     'types': 'BUG,VULNERABILITY',
     'severities': 'BLOCKER,CRITICAL',
-    'statuses': 'OPEN'
+    'statuses': 'OPEN',
+    #'assigned': 'false'
 }
 
 issuesResponse = requests.get("http://localhost:9000/api/issues/search", auth=basicAuth, params=issuesPayload)
@@ -89,13 +90,10 @@ gitlabHeaders = {
 }
 
 for i in range(len(issueData)): #will create an individual issue post in GitLab for each SQ issue found
-    #TODO- look at getting code snippets in GET api/sources/issue_snippets to better fill out description
-    #PENDING SECURITY CONCERNS 
 
     snippetPayload = {
         'issueKey': issueData[i].get('key')
     }
-
     snippetResponse = requests.get("http://localhost:9000/api/sources/issue_snippets", auth=basicAuth, params=snippetPayload)
     print("snippetResponse status code:", snippetResponse.status_code)
 
@@ -103,12 +101,18 @@ for i in range(len(issueData)): #will create an individual issue post in GitLab 
     keyList = list(snippetData.keys())
 
     #the first key needed to get the code is a long string, this gets it easier and stores it for later use as issueFile
+    #the source code is sent and recieved through https by default, this should mitigate some security concerns 
+
     issueFile = keyList[0]
     numSnippetLines = len(snippetData[issueFile]['sources'])
-    mdSource = "<pre><code>"
-    for line in range(numSnippetLines):
-        mdSource = mdSource + '[' + str(snippetData[issueFile]['sources'][line]['line']) + ']' + markdownify.markdownify(snippetData[issueFile]['sources'][line]['code']) + "  \n"
-    mdSource = mdSource + "</code></pre>"
+    mdSource = "<pre>"
+    print(issueData[i].get('line'))
+    for line in range(numSnippetLines): #highlights the line containing the problem code 
+        if snippetData[issueFile]['sources'][line]['line']==issueData[i].get('line'):
+            mdSource = mdSource + '<mark>' + '[' + str(snippetData[issueFile]['sources'][line]['line']) + ']' + markdownify.markdownify(snippetData[issueFile]['sources'][line]['code']) + "</mark>  \n"
+        else:
+            mdSource = mdSource + '[' + str(snippetData[issueFile]['sources'][line]['line']) + ']' + markdownify.markdownify(snippetData[issueFile]['sources'][line]['code']) + "  \n"
+    mdSource = mdSource + "</pre>"
 
     #will get detailed info about the rule that has been violated
     rulesPayload = {
@@ -124,9 +128,8 @@ for i in range(len(issueData)): #will create an individual issue post in GitLab 
     gitlabPayload = {
         'id': 46477662, #This is the id of the gitlab repo
         'title': 'SonarQube - {} : {} '.format(issueData[i].get('type').lower(), issueData[i].get('message').lower()),
-        'description': "SonarQube has detected an issue and generated an automatic bug or vulnerability report  \n  \n {} text: '{}' at line {} in file {}  \n  \n <h3>Code Snippet</h3>  \n  {}  \n  \n <h2>Rule Description:</h2> {}".format(issueData[i].get('type').lower(), issueData[i].get('message').lower()[:-1], issueData[i].get('line'), re.sub("zwadhams_Embedded-Systems-Robotics_AYibu6FRayQ69Q6kvVmx:", "", issueData[i].get('component')), mdSource, ruleData['rule']['mdDesc'])
+        'description': "SonarQube has detected an issue and generated an automatic bug or vulnerability report  \n  \n {} text: '{}' at line {} in file {}  \n  \n <h3>Code Snippet</h3>  \n  {}  \n  \n <h2>Rule Description:</h2> {}".format(issueData[i].get('type').lower(), issueData[i].get('message').lower(), issueData[i].get('line'), re.sub("zwadhams_Embedded-Systems-Robotics_AYibu6FRayQ69Q6kvVmx:", "", issueData[i].get('component')), mdSource, ruleData['rule']['mdDesc'])
     }
-
     #Compresses the payload into json to avoid a 414 post error 
     jsonPayload = json.dumps(gitlabPayload, indent=1)
 
@@ -138,19 +141,19 @@ for i in range(len(issueData)): #will create an individual issue post in GitLab 
     if issuePost.status_code == 201:
         print("-----SUCCESS, GitLab issue created successfully-----")
 
-#TODO- Close all SQ issues that are grabbed in order to prevent duplicates
-# When a new analysis is kicked off any non fixed issues will be set to re-opened status 
-# Using this, I could force them to be closed via a bulk_change and they already will be ignored based on use of the open tag in the issuesPayload
-print("-----Closing out issues in SonarQube-----")
+#Assigns all issues to GitLab SQ user to mark that they have been seen and moved into GitLab
+print("-----Assigning issues to GitLab user in SonarQube-----")
 issueKeys = []
 for issue in range(len(issueData)):
     issueKeys.append(issueData[issue].get('key'))
 
-closePayload = {
-    'issues': issueKeys
+assignPayload = {
+    'issues': issueKeys,
+    'assign': 'GitLab'
 }
-#jprint(issueKeys)
+assignRequest = requests.post("http://localhost:9000/api/issues/bulk_change", auth=basicAuth, params=assignPayload)
+print("assignRequest status code:", assignRequest.status_code)
 
-#bulkCloseRequest = requests.post("http://localhost:9000/api/issues/bulk_change", auth=basicAuth, params=closePayload)
+#TODO-Deal with security hotspots in some way. Mostlikely ignore the SQ workflow and just treat them as normal issues. 
 
 print("SQ/GitLab API Tool Complete")
